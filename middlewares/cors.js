a64828299e82f26a28d458b80040ec00421f7005
Mauzek <artemilliy@gmail.com>
2025-06-07 15:19:45 +0300

    Installed localtunnel lib & change cors headers

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,9 +1,12 @@
 function cors(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // Разрешить доступ всем
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS"
+  );
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization"
+    "Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization, bypass-tunnel-reminder"
   );
 
   // Если это preflight-запрос, завершите его здесь
